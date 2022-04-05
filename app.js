394299567c2652e443798a262cf0e7b46423dc87
Abd El Fattah Amr <amrabdelfattah@hotmail.fr>
2022-04-05 16:44:13 +0200

    First API Call

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let container = document.querySelector('.advices_container')
+
 fetch('https://api.adviceslip.com/advice')
      .then(res => res.json())
-     .then (data => console.log(data.slip.advice))
+     .then (data => {
+            let newParagraph = document.createElement('p'); 
+            newParagraph.innerText = data.slip.advice; 
+            container.appendChild(newParagraph)
+})
