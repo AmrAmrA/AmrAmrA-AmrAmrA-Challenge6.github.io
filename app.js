c9c17d08730506c4be890b8b5af3e9102210c4f9
Abd El Fattah Amr <amrabdelfattah@hotmail.fr>
2022-04-06 19:46:43 +0200

    Final Version

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+// Collect the HTML Elements
 let adviceText           = document.querySelector('.advice_text'); 
 let triggerBtn           = document.querySelector('.btn_trigger'); 
 let adviceNumber         = document.querySelector('.advice_title'); 
 
+// the call to the API 
+// Add the Advice and the Number dynamically 
 triggerBtn.addEventListener('click', launchAdvices)
 function launchAdvices() {
      fetch('https://api.adviceslip.com/advice')
      .then(res => res.json())
      .then (data => {
-            adviceText.innerText = '"' + data.slip.advice + '"'; 
+            adviceText.innerText = '❝ ' + data.slip.advice + ' ❞'; 
             adviceNumber.innerText = 'Advice  '  + ' #' + data.slip.id; 
             adviceNumber.classList.add('.advice_title')
 })
